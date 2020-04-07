acece3b0c2f32949a460353cde7e775f14b9dcb7
Evan Boissonnot <evan@boissonnot.fr>
2020-04-07 19:27:25 +0200

    evo: dal

---
+++ Arround.Architectures/LayersArchitecture/Around.Architectures.Core.Dals/OrderDal.cs
@@ -0,0 +1,23 @@
+﻿using Around.Architectures.Core.Data;
+using Around.Architectures.Core.Interfaces.Dals;
+using Around.Architectures.Core.Models;
+using System;
+
+namespace Around.Architectures.Core.Dals
+{
+    public class OrderDal : IDataAccessLayer<Order>
+    {
+        private DefaultContext _context = null;
+
+        public OrderDal(DefaultContext context)
+        {
+            this._context = context;
+        }
+
+        public void Add(Order order)
+        {
+            this._context.Orders.Add(order);
+            this._context.SaveChanges();
+        }
+    }
+}
